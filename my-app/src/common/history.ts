5b19694c87de8e14af088ff9004237da48c067b7
Koichi Matsuda <k-matsuda@caldia.org>
2019-02-24 11:30:30 +0900

    add localhost:3000

--- my-app/src/common/history.ts
+++ my-app/src/common/history.ts
@@ -1,8 +1,8 @@
 import { createBrowserHistory } from "history";
 
-const hosts = ['localhost:8080', 'caldia-apps.github.io', 'd2otoqn9rs8n0r.cloudfront.net', 'ibchain.github.io']
+const hosts = ['localhost:3000', 'localhost:8080', 'caldia-apps.github.io', 'd2otoqn9rs8n0r.cloudfront.net', 'ibchain.github.io']
 const hid = hosts.indexOf(window.location.hostname)
-const bases = ['', '/IBWallet', '', '/IBWallet/my-app/build']
+const bases = ['', '', '/IBWallet', '', '/IBWallet/my-app/build']
 const basename = bases[hid]
 const browserHistory = createBrowserHistory({ basename });
 
